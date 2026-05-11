c02f3f0454e687b872a67cbb5f6b564c23ce7082
Mariem Mrabet <97696933+Mrabet-Mariem@users.noreply.github.com>
2026-05-11 10:47:55 +0300

    Add logout functionality in logout.php
    Implement logout functionality by destroying the session and clearing the remember-me cookie.

--- admin/logout.php
+++ admin/logout.php
@@ -1,1 +1,11 @@
 
+<?php
+session_start();
+session_destroy();
+
+// Remove remember-me cookie
+setcookie('admin_token', '', time() - 3600, '/', '', false, true);
+
+header('Location: login.php');
+exit;
+?>
